15e77add5756d7720f82e50d8426988933c2329c
Adityo Rancaka <cakdityo@gmail.com>
2016-02-17 14:50:29 +0000

    Wrapping up the CRUD and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, except: [:index, :new]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,13 +13,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article ' + @article.title + ' was successfully created!'
+      flash[:notice] = 'Article \'' + @article.title + '\' was successfully created!'
       redirect_to @article
     else
       render 'new'
@@ -26,9 +25,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = 'Article ' + @article.title + ' was successfully updated!'
+      flash[:notice] = 'Article \'' + @article.title + '\' was successfully updated!'
       redirect_to @article
     else
       render 'edit'
@@ -36,13 +34,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = 'Article ' + @article.title + ' was successfully deleted!'
+    flash[:notice] = 'Article \'' + @article.title + '\' was successfully deleted!'
     redirect_to articles_path
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
