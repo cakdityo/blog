235f2dcc76f0facb452a351b66559b50bdf3e9db
Adityo Rancaka <cakdityo@gmail.com>
2016-02-19 22:11:09 +0700

    Edit user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219030649) do
+ActiveRecord::Schema.define(version: 20160219150113) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20160219030649) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
